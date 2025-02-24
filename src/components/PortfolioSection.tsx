3e8eb83e3971c8d0dcea1ac8bd2d988ffecfc9b5
Gabriel Indok David <indokdavid2020@gmail.com>
2025-02-24 14:51:45 +0100

    Commit

---
+++ src/components/PortfolioSection.tsx
@@ -0,0 +1,7 @@
+const PortfolioSection: React.FC = () => {
+  return (
+    <div className="text-white bg-[#dc7550] rounded-tl-[100%] rounded-tr-[100%] p-10"></div>
+  );
+};
+
+export default PortfolioSection;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import { JSX } from "react";
 import Hero from "../components/Hero";
+import PortfolioSection from "../components/PortfolioSection";
 
 const HomePage = (): JSX.Element => {
   return (
     <div>
       <Hero />
+      <PortfolioSection />
       <div></div>
     </div>
   );
